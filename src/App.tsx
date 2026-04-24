2bf132dcb92f7e82a86d3b6405105efcf186f62a
hitesh.chawla <hitesh.chawla@ta3leem.dev>
2026-04-24 17:11:50 +0530

    update file

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>THIS IS FOR TEST CI/CD</h1>
+      <h1>THIS IS FOR TEST CI/CD FOR REACT AND NEXT JS </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
